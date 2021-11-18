dc1e90dded94534d4840fccf5178c229b5159022
Jimmy <odylic.lin@gmail.com>
2021-11-18 11:35:38 -0800

    tic tac toe

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,7 +1,24 @@
 import * as React from "react";
+import styled from "styled-components";
+
+type LayoutProps = {
+  gap: number;
+};
+
+const Row = styled.div<LayoutProps>`
+  display: flex;
+  flex-direction: row;
+  gap: ${(props) => props.gap}px;
+`;
 
 function Game() {
-  return <div>Hello World</div>;
+  return (
+    <Row gap={20}>
+      <div>Like</div>
+      <div>Share</div>
+      <div>Subscribe</div>
+    </Row>
+  );
 }
 
 export default Game;
