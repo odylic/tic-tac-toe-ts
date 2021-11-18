266577815eb0e363e7b1fedd62f90fceacb3044d
Jimmy <odylic.lin@gmail.com>
2021-11-18 13:18:55 -0800

    game logic

--- src/components/GameState.tsx
+++ src/components/GameState.tsx
@@ -8,7 +8,7 @@ const createBoardState = () => Array<Value>(9).fill(null);
 
 // type script logic
 function calculateWinner(boardState: BoardState) {
-  // winning combinations 
+  // winning combinations
   const winningCombinations = [
     // rows
     [0, 1, 2],
@@ -22,8 +22,18 @@ function calculateWinner(boardState: BoardState) {
     [0, 4, 8],
     [2, 4, 6],
   ];
+  // iterates through the winning combinations
   for (let i = 0; i < winningCombinations.length; i++) {
-    
+    // assigns a,b,c to the elements in each array
+    const [a, b, c] = winningCombinations[i];
+    // checks the value of the boardState's array, if clicked
+    if (
+      boardState[a] &&
+      boardState[a] === boardState[b] &&
+      boardState[a] === boardState[c]
+    )
+      // if it's clicked, it's a winner and all other combinations are clicked, return winner
+      return boardState[a];
   }
-
+  return null;
 }
