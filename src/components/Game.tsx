b5364102bc05af1e15d22931bb4475b6b5507756
Jimmy <odylic.lin@gmail.com>
2021-11-20 13:58:19 -0800

    createprops for each square

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -42,26 +42,29 @@ type Boardprops = {
 function Board({ board, onClick }: Boardprops) {
   const createProps = (square: number): SquareProps => {
     return {
+      // shows the value of the square
       value: board[square],
+      // carries the onClick function of the square
       onClick: () => onClick(square),
     };
   };
   return (
     <Column gap={0}>
       <Row gap={0}>
-        <Square />
-        <Square />
-        <Square />
+        {/* gives each the properties of each square in the board array from 0 to 8 */}
+        <Square {...createProps(0)} />
+        <Square {...createProps(1)} />
+        <Square {...createProps(2)} />
       </Row>
       <Row gap={0}>
-        <Square />
-        <Square />
-        <Square />
+        <Square {...createProps(3)} />
+        <Square {...createProps(4)} />
+        <Square {...createProps(5)} />
       </Row>
       <Row gap={0}>
-        <Square />
-        <Square />
-        <Square />
+        <Square {...createProps(6)} />
+        <Square {...createProps(7)} />
+        <Square {...createProps(8)} />
       </Row>
     </Column>
   );
