0e828e04b8ae23ea207d6f1269d7242484c6fb4f
Jimmy <odylic.lin@gmail.com>
2021-11-20 13:02:13 -0800

    gamestate

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import styled from "styled-components";
+import { useGameState } from "./GameState";
 
 // seems like the main typescript difference
 type LayoutProps = {
